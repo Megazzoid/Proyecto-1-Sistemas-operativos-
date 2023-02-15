1f2c795b0206e321a37368cd56b08f093ee35ed9
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-02-14 22:48:12 -0400

    arreglo semaforos

--- HBO Max Vzla/src/Clases/Main.java
+++ HBO Max Vzla/src/Clases/Main.java
@@ -15,11 +15,12 @@ import java.util.logging.Logger;
  */
 public class Main {
     
-    public static Semaphore semaforo1 = new Semaphore(30);
-    public static Semaphore semaforo2 = new Semaphore(25);
-    public static Semaphore semaforo3 = new Semaphore(50);
-    public static Semaphore semaforo4 = new Semaphore(55);
-    public static Semaphore semaforo5 = new Semaphore(40);
+    public static Semaphore semaforoMax = new Semaphore(30);
+    public static Semaphore semaforoIntro = new Semaphore(30);
+    public static Semaphore semaforoCredits = new Semaphore(25);
+    public static Semaphore semaforoInicio = new Semaphore(50);
+    public static Semaphore semaforoCierra = new Semaphore(55);
+    public static Semaphore semaforoGiro = new Semaphore(40);
     public static Semaphore semaforoMutao = new Semaphore(1);
     
     public static Productores_GOT hilo1;
@@ -29,20 +30,21 @@ public class Main {
     public static Productores_GOT hilo5;
     
     public static volatile int numero = 30;
+    public static volatile int numero2 = 25;
     public static boolean check = true;
-    public static int productor = 3;
+    public static int productor = 1;
     public static String inicio = "0";
                     
     public static void main(String[] args) {
         
-        hilo1 = new Productores_GOT(semaforo1, "Intro");
-        //hilo2 = new Productores_GOT(semaforo2, "Creditos");
-        //hilo3 = new Productores_GOT(semaforo3, "Inicio");
-        //hilo4 = new Productores_GOT(semaforo4, "cierre");
-        //hilo5 = new Productores_GOT(semaforo5, "giro chido");
+        hilo1 = new Productores_GOT(semaforoMax, "intro");
+        hilo2 = new Productores_GOT(semaforoMax, "Creditos");
+        hilo3 = new Productores_GOT(semaforoMax, "Inicio");
+        hilo4 = new Productores_GOT(semaforoMax, "cierre");
+        hilo5 = new Productores_GOT(semaforoMax, "giro chido");
         
         hilo1.start();
-        //hilo2.start();
+        hilo2.start();
         //hilo3.start();
         //hilo4.start();
         //hilo5.start();
