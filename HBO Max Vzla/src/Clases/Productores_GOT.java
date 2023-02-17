2e280de09ed702cfdae5325c73c49f2b4f27f351
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-02-16 23:15:02 -0400

    Listo Ensambladores

--- HBO Max Vzla/src/Clases/Productores_GOT.java
+++ HBO Max Vzla/src/Clases/Productores_GOT.java
@@ -21,12 +21,14 @@ public class Productores_GOT extends Thread{
     Semaphore semaforoCierra;
     Semaphore semaforoGiro;
     Semaphore semaforoLimite;
+    Semaphore introDisp;
     Boolean keep = true;
     int num_prod;    
     int num_prod2;
     int num_prod3;
     int num_prod4;
     int num_prod5;
+    int num_ens;
    
     
     public Productores_GOT(Semaphore semaforoMax, String texto){
@@ -53,12 +55,14 @@ public class Productores_GOT extends Thread{
         this.num_prod5 = productores5; 
     }
     
+    public void ProductoresEnsambladores(int ensa){
+        this.num_ens = ensa; 
+    }
 
     public void producirIntro(){
         for(int i = 0; i <= Main.hilo1.num_prod; i++){
-            System.out.println("Soy Intro");
             if(i == Main.hilo1.num_prod){
-                for(int j = 0; j < 1; j++){
+                for(int j = 0; j < Main.hilo1.num_prod; j++){
                     if(Main.semaforoIntro.availablePermits() > 0){
                         try {
                             Thread.sleep(1000);
@@ -77,9 +81,8 @@ public class Productores_GOT extends Thread{
     
     public void producirCredits(){
         for(int i = 0; i <= Main.hilo2.num_prod2; i++){
-            System.out.println("Soy Creditos");
             if(i == Main.hilo2.num_prod2){
-                for(int f = 0; f < 1; f++){
+                for(int f = 0; f < Main.hilo2.num_prod2; f++){
                     if(Main.semaforoCredits.availablePermits() > 0){
                         try {
                             Thread.sleep(1000);
@@ -98,11 +101,9 @@ public class Productores_GOT extends Thread{
     
     public void producirInicio(){
         for(int i = 0; i <= Main.hilo3.num_prod3; i++){
-            System.out.println("Soy Inicio");
             if(i == Main.hilo3.num_prod3){
-                for(int f = 0; f <= 2; f++){
-                    if(f == 2){
-                        if(Main.semaforoInicio.availablePermits() > 0){
+                for(int f = 0; f < Main.hilo3.num_prod3; f++){
+                    if(Main.semaforoInicio.availablePermits() > 0){
                         try {
                             Thread.sleep(2000);
                             Main.semaforoInicio.acquire(1);
@@ -112,8 +113,8 @@ public class Productores_GOT extends Thread{
                             } catch (InterruptedException ex) {
                                 Logger.getLogger(Productores_GOT.class.getName()).log(Level.SEVERE, null, ex);
                             } 
-                        }
-                }
+                    }
+                
             }
         }
     }
@@ -121,12 +122,9 @@ public class Productores_GOT extends Thread{
        
     public void producirCierra(){
         for(int i = 0; i <= Main.hilo4.num_prod4; i++){
-            System.out.println("Soy Cierra");
             if(i == Main.hilo4.num_prod4){
-                for(int f = 0; f <= 4; f++){
-                    System.out.println(f);
-                    if(f == 4){
-                        if(Main.semaforoCierra.availablePermits() > 0){
+                for(int f = 0; f < Main.hilo4.num_prod4; f++){
+                    if(Main.semaforoCierra.availablePermits() > 0){
                         try {
                             Thread.sleep(4000);
                             Main.semaforoCierra.acquire(1);
@@ -137,19 +135,16 @@ public class Productores_GOT extends Thread{
                                 Logger.getLogger(Productores_GOT.class.getName()).log(Level.SEVERE, null, ex);
                             } 
                         }
-                }
+                
             }
         }
     }
 }
     public void producirGiro(){
-        for(int i = 0; i < Main.hilo5.num_prod5; i++){
-            System.out.println("Soy Giro");
+        for(int i = 0; i <= Main.hilo5.num_prod5; i++){
             if(i == Main.hilo5.num_prod5){
-                for(int f = 0; f <= 2; f++){
-                    System.out.println(f);
-                    if(f == 2){
-                        if(Main.semaforoGiro.availablePermits() > 0){
+                for(int f = 0; f < Main.hilo5.num_prod5; f++){
+                    if(Main.semaforoGiro.availablePermits() > 0){
                         try {
                             Thread.sleep(2000);
                             Main.semaforoGiro.acquire(1);
@@ -160,13 +155,39 @@ public class Productores_GOT extends Thread{
                                 Logger.getLogger(Productores_GOT.class.getName()).log(Level.SEVERE, null, ex);
                             } 
                         }
-                }
+                
             }
         }
     }
 }
+    public void ConstruirSerie(){
+        for(int i = 0; i <= Main.hilo6.num_ens; i++){
+            if(i == Main.hilo6.num_ens){
+                for(int f = 0; f < Main.hilo6.num_ens; f++){
+                    if(Main.semaforoIntro.availablePermits() <= 29 && Main.semaforoCierra.availablePermits() <= 54 && Main.semaforoCredits.availablePermits() <= 24 
+                            && Main.semaforoInicio.availablePermits() <= 49 && Main.semaforoGiro.availablePermits() <= 39 ){
+                        try {
+                            Thread.sleep(5000);
+                            Main.semaforoIntro.release(1);
+                            Main.semaforoCredits.release(1);
+                            Main.semaforoInicio.release(1);
+                            Main.semaforoCierra.release(1);
+                            Main.semaforoGiro.release(1);
+                            Main.semaforoMax.release(5);
+                            System.out.println("Serie producida");
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Productores_GOT.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    } else {
+                        System.out.println("no hay");
+                    }
+                }
+            }
+        }
     
-    @Override
+}
+    
+   @Override
    public void run(){
        while(true){
            for(int i = 0; i < num_prod; i++){
@@ -214,6 +235,16 @@ public class Productores_GOT extends Thread{
            }
            
         }
+            for(int i = 0; i < num_ens; i++){
+               try {
+                   Thread.sleep(1000);
+                   ConstruirSerie();
+               } catch (InterruptedException ex) {
+                   Logger.getLogger(Productores_GOT.class.getName()).log(Level.SEVERE, null, ex);
+               }
+                
+            }
+            
         }
                
     }
