fbd304d07073cfe4999840131ebdae3e3577b650
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-02-23 21:11:50 -0400

    Agregado PJ GOT

---
+++ HBO Max Vzla/src/Clases/ProjectManager_GOT.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import Interfaz.NewJFrame;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author danma
+ */
+public class ProjectManager_GOT extends Thread{
+    
+    private NewJFrame newJFrame = NewJFrame.getInstance();
+    int salario = 7*24;
+    private boolean watchea =false;
+   
+    @Override
+    public void run(){
+        while(true){
+            try {
+                System.out.println(Main.Dias);
+                Main.RickMorty.acquire();
+                Main.sprint.acquire();
+                Thread.sleep(1000);
+                
+                if(Main.Dias > 0){
+                    
+                    Thread.sleep(126);
+                    Main.Dias--;
+                    newJFrame.TotalDias(Integer.toString(Main.Dias));
+                    Main.RickMorty.release();
+                    Main.sprint.release();
+                   
+                }
+                      for(int i = 0; i < 7; i++){
+                          
+                      watchea = true;
+                      Thread.sleep(34);
+                      newJFrame.pjvago("Viendo Rick & Morty");
+                      watchea = false;
+                      Thread.sleep(34);
+                      newJFrame.pjvago("Haciendo Spring Reviews");
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProjectManager_GOT.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+  
+}
+    
+}
+
+
+
+}
