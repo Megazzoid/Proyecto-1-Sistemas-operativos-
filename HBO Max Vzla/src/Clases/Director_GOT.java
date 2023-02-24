d1fb3ec8bcf11c9ea657af8561741fb7c8fc920b
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2023-02-23 23:23:53 -0400

    arreglo GOT

--- HBO Max Vzla/src/Clases/Director_GOT.java
+++ HBO Max Vzla/src/Clases/Director_GOT.java
@@ -6,6 +6,7 @@
 package Clases;
 
 import Interfaz.NewJFrame;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,14 +21,20 @@ public class Director_GOT extends Thread {
     int hora = 42;
     int random = 0;
     int random2 = 0;
-    int salario = 7*1000; 
+    int salario = 30*100; 
+    int ganancias = 0;
+    int audiencia = 0;
+    int vigilar = 0;
+    int descansar = 0;
     
+    @Override
     public void run(){
         while (true){
              if (Main.Dias > 0){
                  try {
                      Main.RickMorty.acquire();
                      Main.RickMorty.release();
+                     newJFrame.DirectorGOT(Integer.toString(salario));
 
                  } catch (InterruptedException ex) {
                      Logger.getLogger(Director_GOT.class.getName()).log(Level.SEVERE, null, ex);
@@ -35,13 +42,42 @@ public class Director_GOT extends Thread {
                 
              } else {
                  
-                 newJFrame.DirectorGOT(Integer.toString(Main.salarioDirector));
+                 Main.audienciaGOT = Main.capNormal + Main.capPlot;
+                 audiencia = Main.audienciaGOT * 980000;
+                 ganancias = audiencia / 150000;
+                 ganancias = ganancias * 100000;
+                 newJFrame.Ganancias(Integer.toString(ganancias));
+                 newJFrame.capDisp(Integer.toString(Main.capNormal));
+                 newJFrame.capPlot(Integer.toString(Main.capPlot));
                  
-                 
-                 
-                 
-                 
+            
              }
+              Random rad = new Random();
+              random = rad.nextInt(43) + 20;
+              random = rad.nextInt(247)+492;
+              
+              vigilar = random+random2;
+              descansar = Dia - (vigilar);
+              
+            try {
+                Thread.sleep(random2);
+                newJFrame.directorvago("Vigilando");
+                Thread.sleep(random);
+                
+                if (Main.hilo7.watchea = true){
+                    Main.hilo7.salario--;
+                    if(Main.hilo7.salario >= 0 ){
+                        newJFrame.sueldoGOT(Integer.toString(Main.hilo7.salario));
+                    }
+                }
+                
+                newJFrame.directorvago("Nada");
+                Thread.sleep(random);
+                Thread.sleep(descansar);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Director_GOT.class.getName()).log(Level.SEVERE, null, ex);
+            }
+              
         }
     }
     
